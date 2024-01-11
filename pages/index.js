11edd8473d17e6d8f534d90731ac0e29daae9efa
Connor McCarl <connormccarl@gmail.com>
2024-01-11 09:33:01 -0800

    fixed security issues

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
+import { Image, Container, Title, Button, Group, Text, List, ThemeIcon, rem } from '@mantine/core';
+import { IconCheck } from '@tabler/icons-react';
+import image from './image.svg';
+
+import classes from '@/styles/Home.module.css';
 
 export default function Home() {
   return (
